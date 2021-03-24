791fa1a0d288ddb383c0a3e1d19999b886b5ccca
Eddie Fann <efann@beowurks.com>
2021-03-24 00:55:24 -0500

    Added Lightbox to all of the relevant images.

--- themes/bstrap_child/js/routines.js
+++ themes/bstrap_child/js/routines.js
@@ -54,6 +54,62 @@ var Routines =
       jQuery(tcSelect).button();
     },
     //----------------------------------------------------------------------------------------------------
+    // When an image is clicked, using jQuery dialog, the picture is displayed like
+    // that of FancyBox.
+    //
+    // Unfortunately, at the moment, I don't have a way to determine the title bar height
+    // before it displays. So I use Chrome Inspect when viewing a dialog box to determine
+    // the height.
+    // From https://stackoverflow.com/questions/8998612/how-to-pass-the-value-undefined-to-a-function-with-multiple-parameters
+    // By the way, to pass undefined as a parameter, use void 0
+    setupLightbox: function ()
+    {
+      let llCheckClass = true;
+      let lcMainContent = "div.main-container";
+      let lcImageClass = "responsive-image-large"
+
+      // Unfortunately, I can't get the title in the template of field.html.twig.
+      // to override the image output.
+      let lcPageTitle = jQuery(document).attr('title').split('|')[0].trim();
+
+      jQuery(lcMainContent + " img").each(function ()
+      {
+        let loImage = jQuery(this);
+        if (!loImage.attr('alt'))
+        {
+          loImage.attr('alt', lcPageTitle);
+        }
+
+        if (!loImage.attr('title'))
+        {
+          loImage.attr('title', lcPageTitle);
+        }
+
+        loImage.removeAttr('width');
+        loImage.removeAttr('height');
+        loImage.removeAttr('style');
+
+        if (llCheckClass)
+        {
+          let lcClasses = loImage.attr('class');
+
+          if ((typeof lcClasses === 'undefined') || (lcClasses.indexOf('responsive-image') < 0))
+          {
+            loImage.addClass(lcImageClass);
+          }
+        }
+
+        if (!loImage.parent().is('a'))
+        {
+          let lcSource = loImage.attr('src');
+          // From https://stackoverflow.com/questions/610406/javascript-equivalent-to-printf-string-format
+          loImage.wrap(`<a href="${lcSource}" data-lightbox="${lcPageTitle}" data-alt="${lcPageTitle}" data-title="${lcPageTitle}"></a>`);
+        }
+      });
+
+    },
+
+    //----------------------------------------------------------------------------------------------------
     showAJAX: function (tlShow)
     {
       var lcAJAX = "#ajax-loading";
